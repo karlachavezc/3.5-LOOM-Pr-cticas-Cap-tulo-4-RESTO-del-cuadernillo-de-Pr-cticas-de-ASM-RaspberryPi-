36be2ed77541a54b721dadac720db6126ce5adba
karla Chávez <79483512+karlachavezc@users.noreply.github.com>
2021-12-13 18:35:01 -0800

    Create pila.s

---
+++ 3.6 CORRIDA, DOCUMENTACIÓN Y LOOM-EJECUCIÓN/pila.s
@@ -0,0 +1,18 @@
+
+//@ Objetivo del programa :
+
+Ejemplo muy sencillo  del uso del stack de la cpu.
+*/
+
+// Basically Push() and Pop() as in stacks. Hmm, interesting stuff for me as a beginner.
+// Basically Push() and Pop() as in stacks. Hmm, interesting stuff for me as a beginner.
+
+//.section // __TEXT, __text
+.global main
+
+main:
+     mov   r0, #2  /* set up r0 */
+     push  {r0}    /* save r0 onto the stack */
+     mov   r0, #3  /* overwrite r0 */
+     pop   {r0}    /* restore r0 to it's initial state */
+     bx    lr      /* finish the program */
